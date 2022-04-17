725764b2d64485f5870e47c13474b5fb0b8c5660
Marshall Baccari <mbaccari@uw.edu>
2022-04-17 15:26:45 -0700

    updated images for portfolio projects

--- src/data/content_option.js
+++ src/data/content_option.js
@@ -1,30 +1,37 @@
+import construction from '../assets/construction.png';
+import join from '../assets/joinIn.png';
+import grill from '../assets/grill.png';
+import notes from '../assets/notes.png'
+
 const meta = {
     title: "Marshall Baccari"
 };
 
+
+
 const dataportfolio = [{
-        img: "https://picsum.photos/600/450/?grayscale",
+        img: construction,
         description: "I choose you (in development)",
         link: "#",
         github:""
     },
     {
-        img: "https://picsum.photos/600/451/?grayscale",
+        img: join,
         description: "Join in",
         link: "https://join-in-app.herokuapp.com/signup",
         github:"https://github.com/kelseybrianne/full-stack-app"
     },
     {
-        img: "https://picsum.photos/600/452/?grayscale",
+        img: grill,
         description: "Grill: Food and friends",
         link: "https://mbaccari.github.io/Grill/",
         github:"https://github.com/mbaccari/Grill"
     },
     {
-        img: "https://picsum.photos/600/449/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-        github:""
+        img: notes,
+        description: "Note taker",
+        link: "https://take-your-notes-dude.herokuapp.com/",
+        github:"https://github.com/mbaccari/Note-Taker"
     }
 ];
 
